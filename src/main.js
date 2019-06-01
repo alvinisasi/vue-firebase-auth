c58906b23848c6da30964186dce35870b377fa01
alvinisasi <azraqazravin@gmail.com>
2019-06-02 06:19:53 +0700

    env.development firebase config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+firebase.initializeApp(firebaseConfig);
